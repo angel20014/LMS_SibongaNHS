4f7206f9874807768d1f8754229caf401dbf2d8d
ANGEL MAE G. UNABIA <angelmaeunabia123@gmail.com>
2025-09-02 23:57:16 +0800

    Add files via upload

---
+++ db/snhs (3).sql
@@ -0,0 +1,535 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 31, 2025 at 10:32 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `snhs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `contact_number` varchar(11) NOT NULL,
+  `security_question` varchar(255) NOT NULL,
+  `security_answer` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `admin_image` varchar(255) NOT NULL,
+  `date_registered` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `firstname`, `middlename`, `lastname`, `address`, `contact_number`, `security_question`, `security_answer`, `username`, `password`, `admin_image`, `date_registered`) VALUES
+(1, 'JESSA', ' UMBAY', 'PADILLO', 'CARCAR CITY, CEBU', '09077934408', 'What is your mother\'s maiden name?', 'jessa1', 'admin', 'admin', 'upload/1754872730_picture2.jpg', '2025-08-10 18:38:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allowed_book`
+--
+
+CREATE TABLE `allowed_book` (
+  `allowed_book_id` int(11) NOT NULL,
+  `qntty_books` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `allowed_book`
+--
+
+INSERT INTO `allowed_book` (`allowed_book_id`, `qntty_books`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allowed_days`
+--
+
+CREATE TABLE `allowed_days` (
+  `allowed_days_id` int(11) NOT NULL,
+  `no_of_days` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `allowed_days`
+--
+
+INSERT INTO `allowed_days` (`allowed_days_id`, `no_of_days`) VALUES
+(1, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barcode`
+--
+
+CREATE TABLE `barcode` (
+  `barcode_id` int(11) NOT NULL,
+  `pre_barcode` varchar(100) NOT NULL,
+  `mid_barcode` int(100) NOT NULL,
+  `suf_barcode` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `barcode`
+--
+
+INSERT INTO `barcode` (`barcode_id`, `pre_barcode`, `mid_barcode`, `suf_barcode`) VALUES
+(1, 'SNHS', 1001, 'LMS'),
+(2, 'SNHS', 1002, 'LMS'),
+(3, 'SNHS', 1002, 'LMS'),
+(4, 'SNHS', 1003, 'LMS'),
+(5, 'SNHS', 1003, 'LMS'),
+(6, 'SNHS', 1003, 'LMS'),
+(7, 'SNHS', 1004, 'LMS'),
+(8, 'SNHS', 1005, 'LMS'),
+(9, 'SNHS', 1005, 'LMS'),
+(10, 'SNHS', 1005, 'LMS'),
+(11, 'SNHS', 1005, 'LMS'),
+(12, 'SNHS', 1005, 'LMS'),
+(13, 'SNHS', 1005, 'LMS'),
+(14, 'SNHS', 1006, 'LMS'),
+(15, 'SNHS', 1006, 'LMS'),
+(16, 'SNHS', 1006, 'LMS'),
+(17, 'SNHS', 1006, 'LMS'),
+(18, 'SNHS', 1006, 'LMS'),
+(19, 'SNHS', 1006, 'LMS'),
+(20, 'SNHS', 1006, 'LMS'),
+(21, 'SNHS', 1006, 'LMS'),
+(22, 'SNHS', 1007, 'LMS'),
+(23, 'SNHS', 1008, 'LMS'),
+(24, 'SNHS', 1009, 'LMS'),
+(25, 'SNHS', 1010, 'LMS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `book_id` int(11) NOT NULL,
+  `book_title` varchar(100) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `author_2` varchar(100) NOT NULL,
+  `author_3` varchar(100) NOT NULL,
+  `author_4` varchar(100) NOT NULL,
+  `author_5` varchar(100) NOT NULL,
+  `book_copies` int(11) NOT NULL,
+  `book_pub` varchar(100) NOT NULL,
+  `publisher_name` varchar(100) NOT NULL,
+  `isbn` varchar(50) NOT NULL,
+  `copyright_year` int(11) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `book_barcode` varchar(100) NOT NULL,
+  `book_image` varchar(100) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `remarks` varchar(100) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`book_id`, `book_title`, `author`, `author_2`, `author_3`, `author_4`, `author_5`, `book_copies`, `book_pub`, `publisher_name`, `isbn`, `copyright_year`, `status`, `book_barcode`, `book_image`, `date_added`, `remarks`, `category_id`) VALUES
+(1, 'English Expressways : Second Year', 'Virginia F. Bermudez', 'Remedios F. Nery', 'Josephine M. Cruz', 'Milagrosa A. San Juan', '', 10, '2010', 'SD Publications, INC.', '978-971-0315-72-7', 2010, 'Old', 'SNHS1LMS', 'IMG_0019.JPG', '2025-08-17 01:06:46', 'Available', 1),
+(2, 'DAYBOOK of Critical Reading and Writing', 'Fran Claggett', 'Louann Reid', 'Ruth Vinz', '', '', 15, '1978', 'Doubleday Canada Limited', '0-669-46432-5', 1978, 'Old', 'SNHS2LMS', 'IMG_0006 - Copy.JPG', '2015-12-14 01:11:06', 'Available', 1),
+(3, 'Getting to Know-Puerto Rico', 'Frances Robins', '', '', '', '', 0, 'Coward-McCann', '1967', '', 0, 'Old', 'SNHS3LMS', '', '2015-12-14 01:21:47', 'Available', 3),
+(4, 'Lotta on Troublemaker Street', 'Astrid Lindgren', '', '', '', '', 0, 'Aladdin Paperbacks', '2001', '0-689-84673-8', 1962, 'Replacement', 'SNHS4LMS', '', '2015-12-14 01:43:06', 'Available', 3),
+(5, 'Great Days of Whailing', 'Henry Beetle Hough', '', '', '', '', 1, '', '', '', 0, 'Damaged', 'SNHS5LMS', '', '2015-12-14 02:05:16', 'Not Available', 4),
+(6, 'Even Big Guys Cry', 'Alex Karras', '', '', '', '', 1, '', '', '', 0, 'Lost', 'SNHS6LMS', '', '2015-12-14 02:05:47', 'Not Available', 2),
+(7, 'Gintong Pamana Wika at Panitikan - Ikalawang Taon', 'Lolita R. Nakpil', 'Leticia F. Dominguez', '', '', '', 4, '2000', 'SD Publications, INC.', '971-07-1885-1', 2000, 'Old', 'SNHS7LMS', 'IMG_0029 - Copy.JPG', '2015-12-14 02:20:36', 'Available', 3),
+(14, 'Florante at Laura', 'Francisco Balagtas', '', '', '', '', 14, '', 'Francisco Balagtas', '12-12341', 1838, 'New', 'SNHS1004LMS', '', '2025-05-19 22:01:29', 'Available', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow_book`
+--
+
+CREATE TABLE `borrow_book` (
+  `borrow_book_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `date_borrowed` datetime NOT NULL,
+  `due_date` datetime NOT NULL,
+  `date_returned` datetime DEFAULT NULL,
+  `borrowed_status` enum('Borrowed','Returned','Pending Return','Accepted','Rejected') NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 1,
+  `penalty_message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `borrow_book`
+--
+
+INSERT INTO `borrow_book` (`borrow_book_id`, `book_id`, `admin_id`, `student_id`, `teacher_id`, `date_borrowed`, `due_date`, `date_returned`, `borrowed_status`, `quantity`, `penalty_message`) VALUES
+(1, 1, 0, 0, 1, '2025-08-30 12:47:27', '2025-08-31 12:47:27', '2025-08-31 16:07:11', 'Returned', 1, ''),
+(2, 7, 0, 1, 0, '2025-08-30 12:49:46', '2025-08-31 12:49:46', '2025-08-31 16:15:58', 'Returned', 1, ''),
+(3, 4, 0, 1, 0, '2025-08-31 16:31:59', '2025-09-01 16:31:59', NULL, 'Borrowed', 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`, `book_id`) VALUES
+(1, 'ENGLISH', 1),
+(2, 'LANGUAGE', 14),
+(3, 'LANGUAGE', 3),
+(4, 'LANGUAGE', 5),
+(5, 'ENGLISH', 2),
+(6, 'LANGUAGE', 6),
+(7, 'LANGUAGE', 7),
+(8, 'LANGUAGE', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penalties`
+--
+
+CREATE TABLE `penalties` (
+  `id` int(11) NOT NULL,
+  `borrow_book_id` int(11) NOT NULL,
+  `borrower_name` varchar(255) NOT NULL,
+  `borrower_type` varchar(100) NOT NULL,
+  `book_title` varchar(255) NOT NULL,
+  `due_date` datetime NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('Pending','Approved','Cancelled') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penalties`
+--
+
+INSERT INTO `penalties` (`id`, `borrow_book_id`, `borrower_name`, `borrower_type`, `book_title`, `due_date`, `message`, `created_at`, `status`) VALUES
+(1, 2, 'LARRY ESPINOSA EMPASIS', 'Student', 'Gintong Pamana Wika at Panitikan - Ikalawang Taon', '2025-08-31 12:49:46', 'Dear LARRY ESPINOSA EMPASIS,\r\n\r\nThis is a reminder that your borrowed book \"Gintong Pamana Wika at Panitikan - Ikalawang Taon\" was due on 2025-08-31 12:49:46.\r\nPlease return it immediately.\r\n\r\nPenalty: You need to clean the library in 2 hours', '2025-08-31 06:42:19', 'Approved');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penalty`
+--
+
+CREATE TABLE `penalty` (
+  `penalty_id` int(11) NOT NULL,
+  `penalty_amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `penalty`
+--
+
+INSERT INTO `penalty` (`penalty_id`, `penalty_amount`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `admin_name` varchar(100) NOT NULL,
+  `detail_action` varchar(100) NOT NULL,
+  `date_transaction` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_book`
+--
+
+CREATE TABLE `return_book` (
+  `return_book_id` int(11) NOT NULL,
+  `borrow_book_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `date_borrowed` datetime NOT NULL,
+  `due_date` datetime NOT NULL,
+  `date_returned` datetime NOT NULL DEFAULT current_timestamp(),
+  `return_status` enum('pending','accepted','rejected') NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `return_book`
+--
+
+INSERT INTO `return_book` (`return_book_id`, `borrow_book_id`, `book_id`, `admin_id`, `student_id`, `teacher_id`, `date_borrowed`, `due_date`, `date_returned`, `return_status`, `quantity`) VALUES
+(1, 1, 1, 1, 0, 1, '2025-08-30 12:47:27', '2025-08-31 12:47:27', '2025-08-31 16:07:11', 'accepted', 1),
+(2, 2, 7, 1, 1, 0, '2025-08-30 12:49:46', '2025-08-31 12:49:46', '2025-08-31 16:15:58', 'accepted', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `studentid_number` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `contact_number` varchar(11) NOT NULL,
+  `student_image` varchar(255) NOT NULL,
+  `security_question` varchar(255) NOT NULL,
+  `security_answer` varchar(255) NOT NULL,
+  `grade_level` enum('Grade 7','Grade 8','Grade 9','Grade 10','Grade 11','Grade 12') NOT NULL,
+  `section` varchar(30) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `date_registered` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `studentid_number`, `firstname`, `middlename`, `lastname`, `address`, `contact_number`, `student_image`, `security_question`, `security_answer`, `grade_level`, `section`, `username`, `password`, `date_registered`) VALUES
+(1, '20250621365', 'LARRY', 'ESPINOSA', 'EMPASIS', 'POBLACION SIBONGA', '09925233142', 'upload/1754988546_user.png', 'What is your pet\'s name?', 'red', 'Grade 9', 'PEARL', 'larry1', 'larry1', '2025-08-12 02:49:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_login_history`
+--
+
+CREATE TABLE `student_login_history` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `login_time` datetime NOT NULL,
+  `logout_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `teacher_id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `contact_number` varchar(11) NOT NULL,
+  `teacher_image` varchar(255) NOT NULL,
+  `security_question` varchar(255) NOT NULL,
+  `security_answer` varchar(255) NOT NULL,
+  `date_registered` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`teacher_id`, `firstname`, `middlename`, `lastname`, `address`, `username`, `password`, `contact_number`, `teacher_image`, `security_question`, `security_answer`, `date_registered`) VALUES
+(1, 'PRINCESS', 'FOB', 'ABELLA', 'POBLACION SIBONGA', 'cess1', 'cess1', '09666455464', 'upload/1755012305_picture-2.jpg', 'What is your favorite color?', 'adobo', '2025-08-12');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `barcode`
+--
+ALTER TABLE `barcode`
+  ADD PRIMARY KEY (`barcode_id`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `borrow_book`
+--
+ALTER TABLE `borrow_book`
+  ADD PRIMARY KEY (`borrow_book_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `penalties`
+--
+ALTER TABLE `penalties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penalty`
+--
+ALTER TABLE `penalty`
+  ADD PRIMARY KEY (`penalty_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`report_id`);
+
+--
+-- Indexes for table `return_book`
+--
+ALTER TABLE `return_book`
+  ADD PRIMARY KEY (`return_book_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `student_login_history`
+--
+ALTER TABLE `student_login_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`teacher_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `barcode`
+--
+ALTER TABLE `barcode`
+  MODIFY `barcode_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `borrow_book`
+--
+ALTER TABLE `borrow_book`
+  MODIFY `borrow_book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `penalties`
+--
+ALTER TABLE `penalties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `penalty`
+--
+ALTER TABLE `penalty`
+  MODIFY `penalty_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `return_book`
+--
+ALTER TABLE `return_book`
+  MODIFY `return_book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `student_login_history`
+--
+ALTER TABLE `student_login_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
